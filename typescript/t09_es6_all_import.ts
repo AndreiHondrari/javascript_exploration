d5ad472fccfd846ee0d8f8fd6814da9b39b11f87
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-05-12 15:22:58 +0300

    es6. all imports from module.

---
+++ typescript/t09_es6_all_import.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+import * as ut from "./ut";
+
+ut.p("all imports\n")
+console.log(`ut.UNIMPORTANT_CONST: ${ut.UNIMPORTANT_CONST}`);
--- typescript/ut.ts
+++ typescript/ut.ts
@@ -3,3 +3,5 @@
 export function p (msg: string): void {
     console.log(`\n### ${msg}`);
 }
+
+export const UNIMPORTANT_CONST = 0xf0;
